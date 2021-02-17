5af43eb8a05c3b9a46f3476f3c4acf7cb988f87e
aneta-s <54153719+aneta-s@users.noreply.github.com>
2021-02-17 17:57:16 +0100

    Updates

---
+++ precache-manifest.ac80496de0f88b650d6e2a6c8093c022.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b084c89db95097f4fc8383393704cc52",
+    "url": "/numbers/index.html"
+  },
+  {
+    "revision": "170f3c556444a3b08865",
+    "url": "/numbers/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "52b7098754095926681d",
+    "url": "/numbers/static/css/main.9a842019.chunk.css"
+  },
+  {
+    "revision": "170f3c556444a3b08865",
+    "url": "/numbers/static/js/2.65b067ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/numbers/static/js/2.65b067ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b7098754095926681d",
+    "url": "/numbers/static/js/main.4bd9b20c.chunk.js"
+  },
+  {
+    "revision": "820278597d5118310b9c",
+    "url": "/numbers/static/js/runtime-main.1b2a1a4c.js"
+  }
+]);
